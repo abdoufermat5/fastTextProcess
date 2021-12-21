b11299682c27ab02c8a46296f5148207a6cfa17b
abdoufermat5 <saabdouyaya@ept.sn>
2021-12-21 18:43:11 +0000

    summarizer done

--- app/src/main/java/com/fermat/textgenres/TextSummarizerFragment.kt
+++ app/src/main/java/com/fermat/textgenres/TextSummarizerFragment.kt
@@ -7,6 +7,8 @@ import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
@@ -21,10 +23,7 @@ class TextSummarizerFragment: Fragment() {
     private var textData: EditText? = null
     private var textResultData: TextView? = null
     private var btnCapture: Button? = null
-    private val btnCopy: Button? = null
-    private var fileUri: Uri? = null
-    private var imageToProcess: Bitmap? = null
-    private val Image_Capture_Code: Int = 12
+    private var btnDelete: Button? = null
 
 
     override fun onCreateView(
@@ -34,10 +33,17 @@ class TextSummarizerFragment: Fragment() {
         val rootView: View = inflater.inflate(R.layout.fragment_text_summarizer, container, false)
         btnCapture = rootView.findViewById(R.id.btnCapture)
         textResultData = rootView.findViewById(R.id.textResultData)
+        btnDelete = rootView.findViewById(R.id.btnCleanText)
         textData = rootView.findViewById(R.id.textData)
         btnCapture!!.setOnClickListener {
             summary(textData!!.text.toString())
 //            ImagePickerHelper.openGalleryOrCamera(this)
+        }
+        btnDelete!!.setOnClickListener {
+            textData!!.text.clear()
+            textResultData!!.text = ""
+            Toast.makeText(this.requireContext(), "Texte effacé!", Toast.LENGTH_SHORT).show()
+            btnDelete!!.visibility = GONE
         }
         // Inflate the layout for this fragment
         return rootView
@@ -47,50 +53,11 @@ class TextSummarizerFragment: Fragment() {
         if (text.isNotEmpty()) {
             val summary = Text2Summary.summarize(text, compressionRate = 0.3F)
             textResultData?.text = summary
+            btnDelete?.visibility = VISIBLE
+            Toast.makeText(this.requireContext(), "Texte résumé à 30%", Toast.LENGTH_LONG).show()
+        }else{
+            Toast.makeText(this.requireContext(), "Incorrecte", Toast.LENGTH_SHORT).show()
         }
-        Toast.makeText(this.requireContext(), "Incorrect", Toast.LENGTH_SHORT).show()
-    }
 
-//    private fun getTextFromImage(bitmap: Bitmap) {
-//        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-//
-//        val image = InputImage.fromBitmap(bitmap, 0)
-//        recognizer.process(image)
-//            .addOnSuccessListener { visionText ->
-//                val text: String = preprocessText(visionText)
-//                Log.d(TAG, "hello buddy: $text")
-//                //textData?.text = text.toString()
-//                btnCapture?.text = getString(R.string.load_text)
-//                btnCopy?.visibility = View.VISIBLE
-//            }
-//            .addOnFailureListener {
-//                Toast.makeText(
-//                    this.requireContext(),
-//                    "Something get wrong!! PLEASE TRY AGAIN",
-//                    Toast.LENGTH_LONG
-//                ).show()
-//            }
-//
-//    }
-//
-//    private fun preprocessText(resultText: Text): String {
-//        if (resultText.textBlocks.size == 0) {
-//            return ""
-//        }
-//        var result = StringBuilder()
-//        for (blockText in resultText.textBlocks) {
-//            for (line in blockText.lines) {
-//                result.append(line.text)
-//            }
-//        }
-//        return result.toString()
-//    }
-
-    private fun renderError(messageResId: Int) {
-        Toast.makeText(
-            this.requireContext(),
-            "Something get wrong!! PLEASE TRY AGAIN",
-            Toast.LENGTH_LONG
-        ).show()
     }
 }
